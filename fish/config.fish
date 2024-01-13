b4079b0a7ecbf2a31a32453ebde3d85bfdadb146
Abhinaya Aryal <aryalabhinaya343@gmail.com>
2024-01-13 20:22:04 +0545

    full path in title of fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,10 @@
 if status is-interactive
   fish_config theme choose Dracula
-  fish_config prompt choose terlar
+  fish_config prompt choose astronaut
+
+  function fish_title
+    set -q argv[1];
+    echo $argv: (fish_prompt_pwd_dir_length=0 prompt_pwd);
+  end
 end
 
