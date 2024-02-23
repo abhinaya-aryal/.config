80a5e8869ff5e67cb34e05a96a8f0272671fef58
Abhinaya Aryal <aryalabhinaya343@gmail.com>
2024-02-23 12:57:05 +0545

    river replaces hyprland

--- hypr/scripts/sleep.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-swayidle -w timeout 300 "dunstify -u critical 'Suspend!' '<span>About to lock the device.</span>'" \
-  timeout 320 'swaylock' \
-  timeout 400 'hyprctl dispatch dpms off' \
-  resume 'hyprctl dispatch dpms on' \
-  timeout 500 'systemctl suspend' \
-  before-sleep 'swaylock' &
