1668e61e2e9fddfd7e2fe21bbd0eb78318f6dd06
Abhinaya Aryal <aryalabhinaya343@gmail.com>
2024-08-20 17:45:54 +0545

    add prettierd as formatter

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -5,15 +5,17 @@ return {
   config = function()
     require("conform").setup({
       formatters_by_ft = {
-        javascript = { "prettier" },
-        javascriptreact = { "prettier" },
-        typescript = { "prettier" },
-        typescriptreact = { "prettier" },
+        javascript = { "prettierd" },
+        javascriptreact = { "prettierd" },
+        typescript = { "prettierd" },
+        typescriptreact = { "prettierd" },
+        html = { "prettierd" },
+        markdown = { "prettierd" },
         ["*"] = { "codespell" },
         ["_"] = { "trim_whitespace" },
       },
       format_on_save = {
-        timeout_ms = 500,
+        timeout_ms = 1000,
       },
       default_format_opts = {
         lsp_format = "fallback",
