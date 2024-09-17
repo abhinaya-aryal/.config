3f7e2046ef7ed53731e5fb124eea655e380ef2b2
Abhinaya Aryal <aryalabhinaya343@gmail.com>
2024-09-17 16:56:30 +0545

    add lazygit
    Config of lazygit is added and toggleterm on nvim is customized with
    lazygit..

--- nvim/lua/plugins/toggleterm.lua
+++ nvim/lua/plugins/toggleterm.lua
@@ -3,7 +3,7 @@ return {
   event = "VeryLazy",
   config = function()
     require("toggleterm").setup({
-      size = 6,
+      size = 2,
       open_mapping = [[<c-\>]],
       hide_numbers = true,
       insert_mapping = true,
@@ -26,5 +26,29 @@ return {
     end
 
     vim.cmd("autocmd! TermOpen term://* lua set_terminal_keymaps()")
+
+    -- open lazygit on <leader>z
+    local Terminal = require('toggleterm.terminal').Terminal
+    local Lazygit = Terminal:new({
+      hidden = true,
+      cmd = "lazygit",
+      display_name = "Lazygit",
+      direction = "float",
+      dir = "git_dir",
+      close_on_exit = true,
+      float_opts = {
+        border = "double",
+      },
+      on_open = function(term)
+        vim.cmd("startinsert!")
+        vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<CR>", { noremap = true, silent = true })
+      end
+    })
+
+    function Lazygit_toggle()
+      Lazygit:toggle()
+    end
+
+    vim.api.nvim_set_keymap("n", "<leader>z", "<cmd>lua Lazygit_toggle()<CR>", { noremap = true, silent = true })
   end,
 }
