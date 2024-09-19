0b8a98885e972c6b25fbb4f9a8feeb3397a1b4fb
Abhinaya Aryal <aryalabhinaya343@gmail.com>
2024-09-19 10:49:40 +0545

    show recording status on lualine

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -2,14 +2,25 @@ return {
   "nvim-lualine/lualine.nvim",
   event = "VeryLazy",
   config = function()
-    local diagnostics = {
-      "diagnostics",
-      symbols = { error = " ", warn = " ", hint = " ", info = "󰋼 " },
-    }
-
     local diff = {
       "diff",
       symbols = { added = " ", modified = " ", removed = " " },
+    }
+
+    local function activeLsp()
+      local buf_ft = vim.api.nvim_buf_get_option(0, "filetype")
+      local clients = vim.lsp.get_clients({ bufnr = 0 })
+      for _, client in ipairs(clients) do
+        if client.config and client.config.filetypes and vim.tbl_contains(client.config.filetypes, buf_ft) then
+          return client.name
+        end
+      end
+      return ""
+    end
+
+    local diagnostics = {
+      "diagnostics",
+      symbols = { error = " ", warn = " ", hint = " ", info = "󰋼 " },
     }
 
     local filename = {
@@ -20,28 +31,6 @@ return {
         readonly = "󰌾",
       },
     }
-
-    local buffers = {
-      "buffers",
-      mode = 1,
-    }
-
-    local function activeLsp()
-      local lsp = "No Lsp"
-      local buf_ft = vim.api.nvim_buf_get_option(0, "filetype")
-      local clients = vim.lsp.get_clients()
-      if next(clients) == nil then
-        return lsp
-      end
-
-      for _, client in pairs(clients) do
-        local filetypes = client.config.filetypes
-        if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
-          return client.name
-        end
-      end
-      return lsp
-    end
 
     local function progress()
       local cur = vim.fn.line(".")
@@ -58,15 +47,19 @@ return {
     require("lualine").setup({
       options = {
         globalstatus = true,
-        disabled_filetypes = { "alpha", "NvimTree", "lazy", "mason", "TelescopePrompt", "toggleterm" },
+        disabled_filetypes = { "alpha", "lazy", "TelescopePrompt", "toggleterm" },
         icons_enabled = true,
         colored = true,
       },
       sections = {
-        lualine_a = { "mode" },
-        lualine_b = { "branch", diff, activeLsp },
-        lualine_c = { diagnostics },
-        lualine_x = { buffers, filename, "filesize" },
+        lualine_a = { "mode", {
+          require("noice").api.statusline.mode.get,
+          cond = require("noice").api.statusline.mode.has,
+          color = { fg = "#ffffff", gui = "bold" }
+        } },
+        lualine_b = { "branch", diff },
+        lualine_c = { activeLsp, diagnostics },
+        lualine_x = { filename, "filesize" },
         lualine_y = { "location" },
         lualine_z = { progress },
       },
