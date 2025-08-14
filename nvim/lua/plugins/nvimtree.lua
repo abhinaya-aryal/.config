67dff17383b28ba5bee1caf11dc489f8f026c620
Abhinaya Aryal <aryalabhinaya343@gmail.com>
2025-08-15 05:39:36 +0545

    update: replace telescope file broswer by nvim-tree

---
+++ nvim/lua/plugins/nvimtree.lua
@@ -0,0 +1,102 @@
+return {
+  "nvim-tree/nvim-tree.lua",
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+  },
+  keys = {
+    { "<leader>e", "<cmd>NvimTreeToggle<cr>", desc = "Toggle NvimTree" },
+  },
+
+  config = function()
+    local api = require("nvim-tree.api")
+
+    require("nvim-tree").setup({
+      hijack_cursor = true,
+      view = {
+        side = "right",
+      },
+      diagnostics = {
+        enable = true,
+        show_on_dirs = true,
+      },
+      modified = {
+        enable = true,
+      },
+      renderer = {
+        icons = {
+          padding = "  ",
+          git_placement = "after",
+          modified_placement = "before",
+        },
+        highlight_git = "all",
+        highlight_modified = "all",
+        root_folder_label = ":~:.",
+        indent_markers = {
+          enable = true,
+        },
+      },
+      filters = {
+        dotfiles = true,
+        custom = { "node_modules", "yarn.lock" },
+      },
+      trash = {
+        cmd = "trash-put"
+      },
+
+      on_attach = function(bufnr)
+        local function opts(desc)
+          return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+        end
+        vim.keymap.set("n", ".", api.tree.change_root_to_node, opts("Set Root"))
+        vim.keymap.set("n", "i", api.node.show_info_popup, opts("Info"))
+        vim.keymap.set("n", "fr", api.fs.rename_sub, opts("Rename: Omit Filename"))
+        vim.keymap.set("n", "v", api.node.open.vertical, opts("Open: Vertical Split"))
+        vim.keymap.set("n", "s", api.node.open.horizontal, opts("Open: Horizontal Split"))
+        vim.keymap.set("n", "p", api.node.navigate.parent_close, opts("Close Directory"))
+        vim.keymap.set("n", "<CR>", api.node.open.edit, opts("Open"))
+        vim.keymap.set("n", "<Tab>", api.node.open.preview, opts("Open Preview"))
+        vim.keymap.set("n", ">", api.node.navigate.sibling.next, opts("Next Sibling"))
+        vim.keymap.set("n", "<", api.node.navigate.sibling.prev, opts("Previous Sibling"))
+        vim.keymap.set("n", "<C-r>", api.node.run.cmd, opts("Run Command"))
+        vim.keymap.set("n", "<bs>", api.tree.change_root_to_parent, opts("Navigate Up"))
+        vim.keymap.set("n", "a", api.fs.create, opts("Create"))
+        vim.keymap.set("n", "bm", api.marks.bulk.move, opts("Move Bookmarked"))
+        vim.keymap.set("n", "B", api.tree.toggle_no_buffer_filter, opts("Toggle Buffer"))
+        vim.keymap.set("n", "c", api.fs.copy.node, opts("Copy"))
+        vim.keymap.set("n", "C", api.tree.toggle_git_clean_filter, opts("Toggle Git Clean"))
+        vim.keymap.set("n", "gp", api.node.navigate.git.prev, opts("Prev Git"))
+        vim.keymap.set("n", "gn", api.node.navigate.git.next, opts("Next Git"))
+        vim.keymap.set("n", "d", api.fs.remove, opts("Delete"))
+        vim.keymap.set("n", "D", api.fs.trash, opts("Trash"))
+        vim.keymap.set("n", "e", api.tree.expand_all, opts("Expand All"))
+        vim.keymap.set("n", "rb", api.fs.rename_basename, opts("Rename: Basename"))
+        vim.keymap.set("n", "F", api.live_filter.clear, opts("Clean Filter"))
+        vim.keymap.set("n", "f", api.live_filter.start, opts("Filter"))
+        vim.keymap.set("n", "?", api.tree.toggle_help, opts("Help"))
+        vim.keymap.set("n", "ca", api.fs.copy.absolute_path, opts("Copy Absolute Path"))
+        vim.keymap.set("n", "H", api.tree.toggle_hidden_filter, opts("Toggle Dotfiles"))
+        vim.keymap.set("n", "I", api.tree.toggle_gitignore_filter, opts("Toggle Git Ignore"))
+        vim.keymap.set("n", "J", api.node.navigate.sibling.last, opts("Last Sibling"))
+        vim.keymap.set("n", "K", api.node.navigate.sibling.first, opts("First Sibling"))
+        vim.keymap.set("n", "m", api.marks.toggle, opts("Toggle Bookmark"))
+        vim.keymap.set("n", "o", api.node.open.edit, opts("Open"))
+        vim.keymap.set("n", "p", api.fs.paste, opts("Paste"))
+        vim.keymap.set("n", "P", api.node.navigate.parent, opts("Parent Directory"))
+        vim.keymap.set("n", "q", api.tree.close, opts("Close"))
+        vim.keymap.set("n", "r", api.fs.rename, opts("Rename"))
+        vim.keymap.set("n", "R", api.tree.reload, opts("Refresh"))
+        vim.keymap.set("n", "sy", api.node.run.system, opts("Run System"))
+        vim.keymap.set("n", "S", api.tree.search_node, opts("Search"))
+        vim.keymap.set("n", "z", api.tree.collapse_all, opts("Collapse All"))
+        vim.keymap.set("n", "x", api.fs.cut, opts("Cut"))
+        vim.keymap.set("n", "y", api.fs.copy.filename, opts("Copy Name"))
+        vim.keymap.set("n", "Y", api.fs.copy.relative_path, opts("Copy Relative Path"))
+      end,
+    })
+
+    -- INFO: Automatically open newly created file
+    api.events.subscribe(api.events.Event.FileCreated, function(file)
+      vim.cmd("edit " .. file.fname)
+    end)
+  end,
+}
--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,32 +1,21 @@
 return {
   "nvim-telescope/telescope.nvim",
   dependencies = {
-    { "nvim-lua/plenary.nvim", "nvim-telescope/telescope-file-browser.nvim" },
+    { "nvim-lua/plenary.nvim" },
   },
   cmd = "Telescope",
   config = function()
     local actions = require("telescope.actions")
-    local fb_actions = require("telescope._extensions.file_browser.actions")
     require("telescope").setup({
       defaults = {
         layout_strategy = "horizontal",
         layout_config = { width = 0.85, height = 0.85 },
         dynamic_preview_title = true,
         mappings = {
-          ["i"] = {
-            ["<esc>"] = actions.close,
-            ["J"] = actions.move_selection_next,
-            ["K"] = actions.move_selection_previous,
-            ["V"] = actions.select_vertical,
-            ["O"] = actions.select_horizontal,
-            ["D"] = actions.preview_scrolling_down,
-            ["U"] = actions.preview_scrolling_up,
-            ["<C-t>"] = false,
-          },
           ["n"] = {
             ["q"] = actions.close,
             ["v"] = actions.select_vertical,
-            ["o"] = actions.select_horizontal,
+            ["s"] = actions.select_horizontal,
             ["d"] = actions.preview_scrolling_down,
             ["u"] = actions.preview_scrolling_up,
           },
@@ -35,62 +24,19 @@ return {
       pickers = {
         buffers = {
           mappings = {
-            ["i"] = {
-              ["D"] = actions.delete_buffer + actions.move_to_top,
-            },
             ["n"] = {
               ["d"] = actions.delete_buffer + actions.move_to_top,
             },
           },
         },
       },
-      extensions = {
-        file_browser = {
-          cwd_to_path = true,
-          hijack_netrw = true,
-          no_ignore = true,
-          mappings = {
-            ["n"] = {
-              ["c"] = fb_actions.create,
-              ["r"] = fb_actions.rename,
-              ["m"] = fb_actions.move,
-              ["y"] = fb_actions.copy,
-              ["d"] = fb_actions.remove,
-              ["o"] = fb_actions.open,
-              ["b"] = fb_actions.goto_parent_dir,
-              ["e"] = fb_actions.goto_home_dir,
-              ["w"] = fb_actions.goto_cwd,
-              ["s"] = fb_actions.change_cwd,
-              ["h"] = fb_actions.toggle_hidden,
-              ["t"] = fb_actions.toggle_all,
-            },
-            ["i"] = {
-              ["C"] = fb_actions.create,
-              ["R"] = fb_actions.rename,
-              ["M"] = fb_actions.move,
-              ["Y"] = fb_actions.copy,
-              ["D"] = fb_actions.remove,
-              ["O"] = fb_actions.open,
-              ["B"] = fb_actions.goto_parent_dir,
-              ["E"] = fb_actions.goto_home_dir,
-              ["W"] = fb_actions.goto_cwd,
-              ["S"] = fb_actions.change_cwd,
-              ["H"] = fb_actions.toggle_hidden,
-              ["T"] = fb_actions.toggle_all,
-              ["<bs>"] = false,
-            },
-          },
-        },
-      },
     })
-    require("telescope").load_extension("file_browser")
   end,
   keys = {
-    { "<leader>tt", "<cmd>Telescope<cr>",              desc = "Telescope" },
-    { "<leader>ff", "<cmd>Telescope find_files<cr>",   desc = "Find File" },
-    { "<leader>ft", "<cmd>Telescope live_grep<cr>",    desc = "Find text in cwd" },
-    { "<leader>fs", "<cmd>Telescope grep_string<cr>",  desc = "Search string under cursor" },
-    { "<leader>fe", "<cmd>Telescope file_browser<cr>", desc = "Browse Files" },
-    { "<leader>fb", "<cmd>Telescope buffers<cr>",      desc = "Find Buffer" },
+    { "<leader>tt", "<cmd>Telescope<cr>",             desc = "Telescope" },
+    { "<leader>ff", "<cmd>Telescope find_files<cr>",  desc = "Find File" },
+    { "<leader>ft", "<cmd>Telescope live_grep<cr>",   desc = "Find text in cwd" },
+    { "<leader>fs", "<cmd>Telescope grep_string<cr>", desc = "Search string under cursor" },
+    { "<leader>fb", "<cmd>Telescope buffers<cr>",     desc = "Find Buffer" },
   },
 }
