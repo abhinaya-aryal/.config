521e3a43797bf9758948ed088b90119be84af5a8
Abhinaya Aryal <aryalabhinaya343@gmail.com>
2025-11-03 10:32:28 +0545

    remove copilot and update lsp in nvim

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,131 +1,115 @@
 return {
-  "neovim/nvim-lspconfig",
-  event = { "FileType" },
-  config = function()
-    local on_attach = function(client, bufnr)
-      if client.server_capabilities.inlayHintProvider then
-        vim.lsp.inlay_hint.enable(true)
-      end
-      vim.api.nvim_set_option_value("omnifunc", "v:lua.vim.lsp,omnifunc", { buf = bufnr })
-      local opts = { buffer = bufnr }
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-      vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-      vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, opts)
-      vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
-      vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, opts)
-      vim.keymap.set("n", "<leader>ld", vim.diagnostic.open_float, opts)
-      vim.keymap.set("n", "<leader>nd", function()
-        vim.diagnostic.jump({ count = 1, float = true })
-      end, opts)
-      vim.keymap.set("n", "<leader>pd", function()
-        vim.diagnostic.jump({ count = -1, float = true })
-      end, opts)
-      vim.keymap.set("n", "<leader>lr", vim.lsp.buf.rename, opts)
-      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
-      vim.keymap.set("n", "<leader>sh", vim.lsp.buf.signature_help, opts)
+	"neovim/nvim-lspconfig",
+	event = { "FileType" },
+	config = function()
+		local on_attach = function(client, bufnr)
+			if client.server_capabilities.inlayHintProvider then
+				vim.lsp.inlay_hint.enable(true)
+			end
+			vim.api.nvim_set_option_value("omnifunc", "v:lua.vim.lsp.omnifunc", { buf = bufnr })
+			local opts = { buffer = bufnr }
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+			vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, opts)
+			vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+			vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, opts)
+			vim.keymap.set("n", "<leader>ld", vim.diagnostic.open_float, opts)
+			vim.keymap.set("n", "<leader>nd", function()
+				vim.diagnostic.jump({ count = 1, float = true })
+			end, opts)
+			vim.keymap.set("n", "<leader>pd", function()
+				vim.diagnostic.jump({ count = -1, float = true })
+			end, opts)
+			vim.keymap.set("n", "<leader>lr", vim.lsp.buf.rename, opts)
+			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+			vim.keymap.set("n", "<leader>sh", vim.lsp.buf.signature_help, opts)
 
-      if client.name == "eslint" then
-        vim.api.nvim_create_autocmd("BufWritePre", {
-          buffer = bufnr,
-          command = "EslintFixAll",
-        })
-      end
-    end
+			if client.name == "eslint" then
+				vim.api.nvim_create_autocmd("BufWritePre", {
+					buffer = bufnr,
+					command = "EslintFixAll",
+				})
+			end
+		end
 
-    local capabilities = vim.lsp.protocol.make_client_capabilities()
-    capabilities.textDocument.completion.completionItem.snippetSupport = true
-    capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
+		local capabilities = vim.lsp.protocol.make_client_capabilities()
+		capabilities.textDocument.completion.completionItem.snippetSupport = true
+		capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 
-    -- defining signs for diagnostics
-    local signs = {
-      { name = "DiagnosticSignError", text = "" },
-      { name = "DiagnosticSignWarn", text = "" },
-      { name = "DiagnosticSignHint", text = "" },
-      { name = "DiagnosticSignInfo", text = "" },
-    }
-    for _, sign in ipairs(signs) do
-      vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = sign.name })
-    end
+		-- defining signs for diagnostics
+		local signs = {
+			{ name = "DiagnosticSignError", text = "" },
+			{ name = "DiagnosticSignWarn", text = "" },
+			{ name = "DiagnosticSignHint", text = "" },
+			{ name = "DiagnosticSignInfo", text = "" },
+		}
+		for _, sign in ipairs(signs) do
+			vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = sign.name })
+		end
 
-    -- diagnostic display options
-    vim.diagnostic.config({
-      virtual_text = true,
-      signs = true,
-      underline = true,
-      float = { border = "rounded" },
-      severity_sort = true,
-      update_in_insert = false,
-    })
+		-- diagnostic display options
+		vim.diagnostic.config({
+			virtual_text = true,
+			signs = true,
+			underline = true,
+			float = { border = "rounded" },
+			severity_sort = true,
+			update_in_insert = false,
+		})
 
-    vim.lsp.config("lua_ls", {
-      on_attach = on_attach,
-      capabilities = capabilities,
-      settings = {
-        Lua = {
-          runtime = {
-            version = "LuaJIT",
-          },
-          workspace = {
-            checkThirdParty = false,
-            library = {
-              vim.env.VIMRUNTIME,
-            },
-          },
-          diagnostics = {
-            globals = { "vim", "mp" },
-          },
-          telemetry = {
-            enable = false,
-          },
-        },
-      },
-    })
+		vim.lsp.config("*", {
+			on_attach = on_attach,
+			capabilities = capabilities,
+		})
 
+		vim.lsp.config("lua_ls", {
+			settings = {
+				Lua = {
+					runtime = {
+						version = "LuaJIT",
+					},
+					workspace = {
+						checkThirdParty = false,
+						library = {
+							vim.env.VIMRUNTIME,
+						},
+					},
+					diagnostics = {
+						globals = { "vim", "mp" },
+					},
+					telemetry = {
+						enable = false,
+					},
+				},
+			},
+		})
 
-    -- C/C++ language server
-    require("lspconfig").clangd.setup({
-      on_attach = on_attach,
-      capabilities = capabilities,
-    })
+		vim.lsp.config("jsonls", {
+			on_attach = on_attach,
+			capabilities = capabilities,
+			settings = {
+				json = {
+					validate = { enable = true },
+					schemaStore = {
+						enable = true,
+						url = "https://www.schemastore.org/api/json/catalog.json",
+					},
+				},
+			},
+		})
 
-    -- Zig language server
-    require("lspconfig").zls.setup({
-      on_attach = on_attach,
-      capabilities = capabilities,
-    })
+		vim.lsp.enable("lua_ls")
 
-    -- Typescript language server
-    -- vim.lsp.config("ts_ls").setup({
-    --   on_attach = on_attach,
-    --   capabilities = capabilities,
-    -- })
-    vim.lsp.enable("ts_ls")
+		vim.lsp.enable("clangd")
 
-    -- Tailwindcss language server
-    require("lspconfig").tailwindcss.setup({
-      on_attach = on_attach,
-      capabilities = capabilities,
-    })
+		vim.lsp.enable("zls")
 
-    -- Eslint
-    require("lspconfig").eslint.setup({
-      on_attach = on_attach,
-      capabilities = capabilities,
-    })
+		vim.lsp.enable("tailwindcss")
 
-    -- Json language server
-    require("lspconfig").jsonls.setup({
-      on_attach = on_attach,
-      capabilities = capabilities,
-      settings = {
-        json = {
-          validate = { enable = true },
-          schemaStore = {
-            enable = true,
-            url = "https://www.schemastore.org/api/json/catalog.json",
-          },
-        },
-      },
-    })
-  end,
+		vim.lsp.enable("ts_ls")
+
+		vim.lsp.enable("eslint")
+
+		vim.lsp.enable("jsonls")
+	end,
 }
